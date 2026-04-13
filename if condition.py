6da8ce2fc20a22cbf8bd2e276f25bea995aa957c
unknown <mohanatce@gmail.com>
2026-04-13 23:23:49 +0530

    1st commit

---
+++ if condition.py
@@ -0,0 +1,16 @@
+n=input("Enter Username:")
+if n == "hariharan.1598@thealschool.org" :
+    p=input("Enter password:")
+    if p == "hari@2008":
+        print("correct password")
+        d=input("important document password:")
+        if d== "operation dark":
+            print("No new meassage yet")
+        else :
+            print("Incorrect password system shutdown")
+    else :
+        print("incorrect password")
+else:
+    print("Incorrect username")
+    
+    
---
+++ sample 1.py
@@ -0,0 +1,5 @@
+n=(input("Enter Username :")
+   if n = hariharan :
+   print("hi")
+   else:
+       print("incorrect username")
